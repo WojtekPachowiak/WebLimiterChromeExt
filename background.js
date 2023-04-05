5dc1106aa660c24a900b8af8ecefde06f3783e6f
WojtekPachowiak <wojtekpaszo@gmail.com>
2023-04-05 20:10:09 +0200

    second version for version 3

--- background.js
+++ background.js
@@ -1,19 +1,25 @@
-chrome.webRequest.onBeforeRequest.addListener(
+chrome.scripting.registerServiceWorker({
+    url: "background.js",
+    interceptRequest: true
+  });
+  
+  chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-        var url = details.url;
-
-        if (url.includes("messenger.com")) {
-            var today = new Date().toLocaleDateString();
-            var lastAccessed = localStorage.getItem(url);
-            if (lastAccessed === today) {
-            return { cancel: true };
-            } else {
-            localStorage.setItem(url, today);
-            return { cancel: false };
-            }
+      var url = details.url;
+      if (url.includes("messenger.com")) {
+        var today = new Date().toLocaleDateString();
+        var lastAccessed = localStorage.getItem(url);
+        if (lastAccessed === today) {
+          return { cancel: true };
+        } else {
+          localStorage.setItem(url, today);
+          return { cancel: false };
         }
+      } else {
+        return { cancel: false };
+      }
     },
-    { urls: ["<all_urls>"] },
+    { urls: ["<all_urls>"], types: ["main_frame"] },
     ["blocking"]
   );
   
--- content.js
+++ content.js
@@ -1,4 +1,3 @@
-var body = document.getElementsByTagName("body")[0];
 var message = document.createElement("div");
 message.innerHTML = "You have already accessed this website today.";
-body.insertBefore(message, body.firstChild);
+document.body.prepend(message);
